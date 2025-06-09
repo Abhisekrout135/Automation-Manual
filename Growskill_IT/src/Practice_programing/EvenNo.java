c62fd01f8bdff449f9080b9cb238881ffb5a106b
Abhisek <abhisekrout135@gmail.com>
2025-06-09 23:53:06 +0530

    New

---
+++ Growskill_IT/src/Practice_programing/EvenNo.java
@@ -0,0 +1,16 @@
+package Practice_programing;
+
+public class EvenNo {
+	    public static void main(String[] args) {
+	        int sum = 0;
+
+	        for (int i = 2; i <= 50; i += 2) {
+	            sum += i;
+	        }
+
+	        System.out.println("Sum of even numbers between 1 and 50 is: " + sum);
+	    }
+	}
+
+
+
---
+++ Growskill_IT/src/Practice_programing/FirstTenOddNo.java
@@ -0,0 +1,19 @@
+package Practice_programing;
+
+public class FirstTenOddNo {
+
+	public static void main(String[] args) {
+		        int count = 0;
+		        int number = 1;
+
+		        while (count < 10) {
+		            System.out.print(number + " ");
+		            number += 2;
+		            count++;
+		        }
+		    }
+		
+
+	}
+
+
